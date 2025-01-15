5b2c81b16be9bc56cbc1dc5ff86b0046e5b6d7c2
linusha <mail@linushagemann.de>
2025-01-15 13:07:44 +0100

    Exchange `chart.js` for `plotly`

--- index.js
+++ index.js
@@ -14,128 +14,36 @@ fetch('sample_data.json')
         }
 
         function aggregateVotes(sample) {
-            const votes = sample.map(item => [item[0], item[2]]);
-
-            const votesByAgeGroup = votes.reduce((acc, [age, party]) => {
-                let ageGroup = acc.find(entry => entry.age === age);
-
-                if (!ageGroup) {
-                    ageGroup = { age, preferences: {} };
-                    acc.push(ageGroup);
-                }
-                ageGroup.preferences[party] = (ageGroup.preferences[party] || 0) + 1;
-
+            const votes = sample.map(item => item[2]);
+            const votesByParty = votes.reduce((acc, vote) => {
+                if (acc[vote]) acc[vote] += 1;
+                else acc[vote] = 1;
                 return acc;
-            }, []);
-
-            return votesByAgeGroup;
+            }, {})
+            return votesByParty;
         }
 
         function updateBarChart(votes) {
-            chart?.destroy();
-            chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: ['CDU/CSU', 'SPD', 'Afd', 'FDP', 'LINKE', 'B90', "sonstige"],
-                    datasets: [{
-                        label: '18-24',
-                        data: [
-                            votes.find(v => v.age == "18").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "18").preferences["SPD"],
-                            votes.find(v => v.age == "18").preferences["Afd"],
-                            votes.find(v => v.age == "18").preferences["FDP"],
-                            votes.find(v => v.age == "18").preferences["LINKE"],
-                            votes.find(v => v.age == "18").preferences["B90"],
-                            votes.find(v => v.age == "18").preferences["sonstige"]
-                        ]
-                    },
+            Plotly.newPlot("holder", // elem
+                [
                     {
-                        label: '25-34',
-                        data: [
-                            votes.find(v => v.age == "25").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "25").preferences["SPD"],
-                            votes.find(v => v.age == "25").preferences["Afd"],
-                            votes.find(v => v.age == "25").preferences["FDP"],
-                            votes.find(v => v.age == "25").preferences["LINKE"],
-                            votes.find(v => v.age == "25").preferences["B90"],
-                            votes.find(v => v.age == "25").preferences["sonstige"]
-                        ]
-                    },
-                    {
-                        label: '25-34',
-                        data: [
-                            votes.find(v => v.age == "25").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "25").preferences["SPD"],
-                            votes.find(v => v.age == "25").preferences["Afd"],
-                            votes.find(v => v.age == "25").preferences["FDP"],
-                            votes.find(v => v.age == "25").preferences["LINKE"],
-                            votes.find(v => v.age == "25").preferences["B90"],
-                            votes.find(v => v.age == "25").preferences["sonstige"]
-                        ]
-                    },
-                    {
-                        label: '35-44',
-                        data: [
-                            votes.find(v => v.age == "35").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "35").preferences["SPD"],
-                            votes.find(v => v.age == "35").preferences["Afd"],
-                            votes.find(v => v.age == "35").preferences["FDP"],
-                            votes.find(v => v.age == "35").preferences["LINKE"],
-                            votes.find(v => v.age == "35").preferences["B90"],
-                            votes.find(v => v.age == "35").preferences["sonstige"]
-                        ]
-                    },
-                    {
-                        label: '45-59',
-                        data: [
-                            votes.find(v => v.age == "45").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "45").preferences["SPD"],
-                            votes.find(v => v.age == "45").preferences["Afd"],
-                            votes.find(v => v.age == "45").preferences["FDP"],
-                            votes.find(v => v.age == "45").preferences["LINKE"],
-                            votes.find(v => v.age == "45").preferences["B90"],
-                            votes.find(v => v.age == "45").preferences["sonstige"]
-                        ]
-                    },
-                    {
-                        label: '60-69',
-                        data: [
-                            votes.find(v => v.age == "60").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "60").preferences["SPD"],
-                            votes.find(v => v.age == "60").preferences["Afd"],
-                            votes.find(v => v.age == "60").preferences["FDP"],
-                            votes.find(v => v.age == "60").preferences["LINKE"],
-                            votes.find(v => v.age == "60").preferences["B90"],
-                            votes.find(v => v.age == "60").preferences["sonstige"]
-                        ]
-                    },
-                    {
-                        label: '70+',
-                        data: [
-                            votes.find(v => v.age == "70").preferences["CDU/CSU"],
-                            votes.find(v => v.age == "70").preferences["SPD"],
-                            votes.find(v => v.age == "70").preferences["Afd"],
-                            votes.find(v => v.age == "70").preferences["FDP"],
-                            votes.find(v => v.age == "70").preferences["LINKE"],
-                            votes.find(v => v.age == "70").preferences["B90"],
-                            votes.find(v => v.age == "70").preferences["sonstige"]
-                        ]
+                        "x": ["CDU/CSU", "SPD", "AfD", "Grüne", "FDP", "Linke", "Sonstige"], // labels
+                        "y": [
+                            votes["CDU/CSU"],
+                            votes["SPD"],
+                            votes["Afd"],
+                            votes["B90"],
+                            votes["FDP"],
+                            votes["LINKE"],
+                            votes["sonstige"]
+                        ], // values
+                        "type": "bar"
                     }
-                    ]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        x: {
-                            stacked: true,
-                        },
-                        y: {
-                            stacked: true
-                        }
-                    }
-                }
-            });
-        }
+                ], // data
+                { "width": 500, "height": 350}, // layout
+                {displayModeBar: false}) // config
+        
+        };
 
         function redraw() {
             const sample = drawSample();
